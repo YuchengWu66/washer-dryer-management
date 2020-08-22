9fb07763f72420be0b4d8a19c18cd668531e681f
yuchengwu <wuyuchen0208@gmail.com>
2020-08-21 17:12:35 -0700

    fifth commit

--- app/src/main/java/com/laioffer/washerdrymanagement/MainActivity.java
+++ app/src/main/java/com/laioffer/washerdrymanagement/MainActivity.java
@@ -1,14 +1,30 @@
 package com.laioffer.washerdrymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationManager {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        navigateTo(new OnBoardingBaseFragment());
     }
+
+    @Override
+    public void navigateTo(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.first_fragment, fragment, null)
+                .addToBackStack(null)
+                .commit();
+    }
+
+
+    // add Fragment to the activity
+    //getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, MainFragment.newInstance()).commit();
 }
+
