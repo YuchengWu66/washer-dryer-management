d3cc08605d5053dd639683993775bd4eb9ddddf4
Yanbo Jiang <j654563613@icloud.com>
2020-09-03 00:26:18 -0400

    tryFixPassingValue3

--- app/src/main/java/com/laioffer/washerdrymanagement/ui/home/HomeAdapter.java
+++ app/src/main/java/com/laioffer/washerdrymanagement/ui/home/HomeAdapter.java
@@ -33,6 +33,7 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
     public int filter;
     private Context context;
     private Map<Integer, String> map;
+
     public HomeAdapter(Context context, int filter) {
         this.context = context;
         this.filter = filter;
